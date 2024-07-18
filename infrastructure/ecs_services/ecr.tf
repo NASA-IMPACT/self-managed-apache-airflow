18a2740df1e392e50c6300321462426dc92b7ddd
Abdelhak Marouane <am0089@uah.edu>
2024-07-18 09:41:52 -0500

    Revert to use X86 architecture

--- infrastructure/ecs_services/ecr.tf
+++ infrastructure/ecs_services/ecr.tf
@@ -65,11 +65,28 @@ resource "null_resource" "build_worker_ecr_image" {
 
   provisioner "local-exec" {
     command = <<EOF
+        exit_status () {
+        STATUS=$?
+        RED='\033[0;31m' # Red Text
+        GREEN='\033[0;32m' # Green Text
+        BLUE='\033[0;34m' # Blue Text
+        NC='\033[0m' # No Color
+        if [ "$STATUS" -eq 0 ]; then
+          printf "\n $GREEN WORKER Docker Image Successfull: $1  $NC \n"
+        else
+          printf "\n $RED WORKER Docker Image failed: $1 $NC \n" >&2
+          exit $STATUS
+        fi
+      }
+
           cd ../${path.root}
           aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${var.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com
-          docker buildx build -t ${aws_ecr_repository.worker_airflow.repository_url}:latest -f airflow_worker/Dockerfile .
+          docker buildux build -t ${aws_ecr_repository.worker_airflow.repository_url}:latest -f airflow_worker/Dockerfile .
+          exit_status "build"
           docker push ${aws_ecr_repository.worker_airflow.repository_url}:latest
+          exit_status "push"
           cd -
+
        EOF
   }
 }
